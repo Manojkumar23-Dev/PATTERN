f4e80d8049a0bcad2f2d4900283dd32830137768
Manoj Kumar <154863849+Manojkumar23-Dev@users.noreply.github.com>
2024-11-19 12:39:00 +0530

    Add files via upload

---
+++ SQUARE.java
@@ -0,0 +1,18 @@
+package pattern;
+import java.util.Scanner;
+public class SQUARE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++)
+		{
+			for(int j=0;j<a;j++)
+			{
+				System.out.print(" venu v king ");
+			}
+			System.out.println();
+		}
+	}
+}
